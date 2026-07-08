6b4794d983a705d9f04fdc8c12d98180efb1f0c2
Marc Beck König <mbkj@tutamail.com>
2026-07-08 20:05:34 +0200

    add display mask types to gt-ui-types

--- crates/gt-ui-types/src/lib.rs
+++ crates/gt-ui-types/src/lib.rs
@@ -1,9 +1,11 @@
+pub mod display_mask;
 pub mod event_marker_visibility;
 pub mod generated_marker_visibility;
 pub mod highlight;
 pub mod query_matches;
 pub mod visibility;
 
+pub use display_mask::{DisplayCategory, DisplayMask};
 pub use event_marker_visibility::EventMarkerVisibility;
 pub use generated_marker_visibility::GeneratedMarkerVisibility;
 pub use highlight::{DataPointRef, HighlightScope, MapHighlight, MatchHighlight};
